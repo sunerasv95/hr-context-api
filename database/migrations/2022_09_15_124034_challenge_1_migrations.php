7ee3dc57baaae5a832df7e02851cd1671e0a855e
sunerasv95 <sachisune@gmail.com>
2022-09-16 01:31:22 +0530

    challenge 1 - migration file updated

--- database/migrations/2022_09_15_124034_challenge_1_migrations.php
+++ database/migrations/2022_09_15_124034_challenge_1_migrations.php
@@ -41,8 +41,8 @@ return new class extends Migration
         Schema::create('c1_attendences', function (Blueprint $table) {
             $table->id();
             $table->date('checked_date');
-            $table->time('checked_time');
-            $table->tinyInteger('is_checkin')->default(1);
+            $table->time('checkin_at');
+            $table->time('checkout_at');
             $table->timestamps();
         });
 
@@ -52,14 +52,22 @@ return new class extends Migration
         });
 
         Schema::table('c1_schedules', function (Blueprint $table) {
-            $table->foreignId('employee_id')->references('id')->on('c1_employees');
-            $table->foreignId('location_id')->references('id')->on('c1_locations');
-            $table->foreignId('shift_id')->references('id')->on('c1_shifts');
+            $table->foreignId('employee_id')->after('scheduled_date')->references('id')->on('c1_employees');
+            $table->foreignId('location_id')->after('employee_id')->references('id')->on('c1_locations');
+            $table->foreignId('shift_id')->after('location_id')->references('id')->on('c1_shifts');
         });
 
         Schema::table('c1_attendence_faults', function (Blueprint $table) {
-            $table->foreignId('employee_id')->references('id')->on('c1_employees');
-            $table->foreignId('attendence_id')->references('id')->on('c1_attendences');
+            $table->foreignId('employee_id')->after('id')->references('id')->on('c1_employees');
+            $table->foreignId('attendence_id')->after('employee_id')->references('id')->on('c1_attendences');
+        });
+
+        Schema::table('c1_employees', function (Blueprint $table) {
+            $table->integer('employee_code')->unique()->after('employee_name');
+        });
+
+        Schema::table('c1_attendences', function (Blueprint $table) {
+            $table->foreignId('employee_id')->after('id')->references('id')->on('c1_employees');
         });
     }
 
@@ -79,6 +87,10 @@ return new class extends Migration
         Schema::table('c1_attendence_faults', function (Blueprint $table) {
             $table->dropForeign('c1_attendence_faults_employee_id_foreign');
             $table->dropForeign('c1_attendence_faults_attendence_id_foreign');
+        });
+
+        Schema::table('c1_attendences', function (Blueprint $table) {
+            $table->dropForeign('c1_attendences_employee_id_foreign');
         });
 
         Schema::dropIfExists('c1_employees');
